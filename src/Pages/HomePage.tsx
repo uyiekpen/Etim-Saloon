ef1714c15c501f4422bd8e247e4ff0afdca29bf6
uyiekpen <uyiekpenelizabeth@gmail.com>
2023-01-03 21:54:57 +0100

    prices

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../Components/Home";
+import Prices from "../Components/Prices";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Home />
+      <Prices />
+    </div>
+  );
+};
+
+export default HomePage;
--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "../Pages/Home";
 import Cart from "../Pages/cart";
 import About from "../Pages/About";
 import ProductDetails from "../Pages/productDetails";
 import Shop from "../Pages/Shop";
+import HomePage from "../Pages/HomePage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="home" />} />
-      <Route path="home" element={<Home />} />
+      <Route path="home" element={<HomePage />} />
       <Route path="cart" element={<Cart />} />
       <Route path="about" element={<About />} />
       <Route path="product/:id" element={<ProductDetails />} />
